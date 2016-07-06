296e1ea6df7f466e0a8488237bdcae93d5996426
Hoang Tien Dat <dat.snor2911@gmail.com>
2016-07-07 01:50:59 +0700

    Initial Commit

---
+++ MovieApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieApp2
+//
+//  Created by Dat Hoang on 7/7/16.
+//  Copyright © 2016 Dat Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
