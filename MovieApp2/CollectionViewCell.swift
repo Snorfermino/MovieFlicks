1d8d69eb4e7062b572fc383c04b28892e1912468
System Administrator <root@Thangs-MacBook-Pro.local>
2016-07-12 19:42:16 +0700

    update

---
+++ MovieApp2/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  MovieApp2
+//
+//  Created by Thang Nguyen on 7/11/16.
+//  Copyright © 2016 Dat Hoang. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+}
